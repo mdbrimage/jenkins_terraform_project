50dc66e8712447bc3353a03e8681674904a16ba1
mdbrimage <mbrimage58@gmail.com>
2024-02-20 18:18:40 +0000

    Final solution AWS and jenkins resources deployed with Terraform

--- terraform_LUIT/main.tf
+++ terraform_LUIT/main.tf
@@ -18,25 +18,31 @@ resource "aws_instance" "terraform_1" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.LUIT_terraform_SG.id]
-
+  
   tags = {
     Name = "LUIT_Terraform_instance"
   }
 
 
-  #Bootstrap Jenkins 
-  user_data = <<EOF
-#!/bin/bash
-yum update -y
-yum install -y java-1.8.0-openjdk
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install -y jenkins
-systemctl start jenkins
-systemctl enable jenkins
-EOF
+#Bootstrap Jenkins 
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt install openjdk-11-jre -y
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install jenkins -y
+  sudo systemctl enable jenkins 
+  sudo systemtl start jenkins
+  EOF
 
+  user_data_replace_on_change = true
 }
+
 
 #Create security group 
 resource "aws_security_group" "LUIT_terraform_SG" {
@@ -83,10 +89,10 @@ resource "aws_security_group" "LUIT_terraform_SG" {
   }
 }
 
-resource "aws_s3_bucket" "jenkins_artifacts" {
-  bucket = "jenkins_artifacts_mdb2005_2024_02_19"
+resource "aws_s3_bucket" "jenkins-artifacts" {
+  bucket = "jenkins-artifacts-mdb2005-2024-02-19"
 
   tags = {
-    Name        = "jenkins_artifacts_mdb2005_2024_02_19"
+    Name = "jenkins_artifacts_mdb2005_2024_02_19"
   }
 }
